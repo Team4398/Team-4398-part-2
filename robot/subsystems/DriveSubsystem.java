faff13773612a853912f1446c214d5703912d869
litwild <48494367+litwild@users.noreply.github.com>
2019-03-12 16:53:49 -0400

    Add files via upload

---
+++ robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+
+/**
+ * Add your docs here.
+ */
+public class DriveSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public Spark leftMotorPort;
+  public Spark rightMotorPort;
+
+  public DifferentialDrive drive = new DifferentialDrive(leftMotorPort, rightMotorPort);
+  public DriveSubsystem() {
+    leftMotorPort = new Spark(RobotMap.leftMotorPort);
+    rightMotorPort = new Spark(RobotMap.rightMotorPort);
+
+  }
+  public void manualDrive(double move, double turn) {
+    
+    drive.arcadeDrive(move, turn);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
